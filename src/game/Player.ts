d8cc648496af98d20fc58cc5dc68985549583320
Daniel Hellstern <dhellstern@outlook.com>
2020-06-25 23:53:50 -0700

    Implemented player gamemodes

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -1,6 +1,15 @@
+enum Gamemode {
+    Survival = 0b000,
+    Creative = 0b001,
+    Adventure = 0b010,
+
+    Hardcore = 0b100
+}
+
 export class Player {
     public Username: string;
     public UUID: string;
+    public Gamemode: number
 
     constructor(username: string, uuid?: string) {
         this.Username = username;
