7209866cd88b833a60f2e19eac4763c3fef7133b
Daniel Hellstern <dhellstern@outlook.com>
2020-06-28 23:37:59 -0700

    Removed currently unused player attributes Will be re-added at a later date

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -14,10 +14,6 @@ export class Player {
     public Username: string;
     public UUID: UUID;
     public Gamemode: number;
-    public Health: number;
-    public Hunger: number;
-    public Saturation: number;
-    public XP: number;
 
     constructor(username: string, uuid?: UUID) {
         this.Username = username;
@@ -25,10 +21,6 @@ export class Player {
 
         // Defaults
         this.Gamemode = Gamemode.Survival;
-        this.Health = 20;
-        this.Hunger = 20;
-        this.Saturation = 0;
-        this.XP = 0;
     }
 
     public async Save() {
@@ -39,21 +31,7 @@ export class Player {
                 uuid: this.UUID.Format()
             }, {
                 $set: {
-                    gamemode: this.Gamemode,
-                    health: this.Health,
-                    hunger: this.Hunger,
-                    saturation: this.Saturation,
-                    xp: this.XP,
-                    inventory: [],
-                    position: {
-                        x: 0,
-                        y: 0,
-                        z: 0
-                    },
-                    rotation: {
-                        horizontal: 0,
-                        vertical: 0
-                    }
+                    gamemode: this.Gamemode
                 }
             }, {
                 upsert: true
@@ -65,16 +43,12 @@ export class Player {
         // Retrieve all existing player data
         const playerDocument: IPlayerDocument = await PlayerModel.findOne({
             uuid: uuid.Format()
-        }, [ "gamemode", "health", "hunger", "saturation", "xp" ]);
+        }, [ "gamemode" ]);
 
         if (playerDocument) {
             // Import the player data into a new Player object
             const player: Player = new Player(username, uuid);
             player.Gamemode = playerDocument.gamemode;
-            player.Health = playerDocument.health;
-            player.Hunger = playerDocument.hunger;
-            player.Saturation = playerDocument.saturation;
-            player.XP = playerDocument.xp;
 
             return player;
         } else return new Player(username, uuid);
