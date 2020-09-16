fb5dbd503d2d36aacc45dd74a8269d569a60ecbe
Daniel Hellstern <dhellstern@outlook.com>
2020-09-15 22:58:38 -0700

    Added offline mode UUID resolution
    This makes it not offline, since it reaches out to the internet

--- src/protocol/states/login/LoginStartPacket.ts
+++ src/protocol/states/login/LoginStartPacket.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { AxiosResponse } from "axios";
 import { Client } from "../../../protocol/Client";
 import { IServerboundPacket } from "../../Packet";
 import { ReadableBuffer } from "../../ReadableBuffer";
@@ -6,8 +8,15 @@ import { Player } from "../../../game/Player";
 import { LoginSuccessPacket } from "./LoginSuccessPacket";
 import { EncryptionRequestPacket } from "./EncryptionRequestPacket";
 import { JoinGamePacket } from "../play/JoinGamePacket";
+import { DisconnectPacket } from "./DisconnectPacket";
 import { UUID } from "../../../game/UUID";
 import { Settings, MinecraftConfigs } from "../../../Configuration";
+import { ChatComponentFactory } from "../../../game/chat/ChatComponentFactory";
+
+interface ProfileResponse {
+    name: string,
+    id: string
+}
 
 export class LoginStartPacket implements IServerboundPacket {
     private _Client: Client;
@@ -33,14 +42,25 @@ export class LoginStartPacket implements IServerboundPacket {
             // Begin the encryption/authentication process
             this._Client.Queue(new EncryptionRequestPacket(this._Client));
         } else {
-            this._Client.Player = new Player(username, UUID.Generate());
+            // Authenticate the client
+            let res: AxiosResponse<ProfileResponse> = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`);
 
-            console.log(`Online mode is off, allowing alleged player ${this._Client.Player.Username} to connect`);
+            // Confirm client authentication succeeded
+            if (res.status == 200) {
+                // Create an unauthenticated player object
+                this._Client.Player = await Player.Load(username, new UUID(res.data.id));
 
-            // Prepare the player to join
-            this._Client.Queue(new SetCompressionPacket(this._Client));
-            this._Client.Queue(new LoginSuccessPacket(this._Client));
-            this._Client.Queue(new JoinGamePacket(this._Client));
+                console.log(`Online mode is off, allowing alleged player ${this._Client.Player.Username} to connect`);
+
+                // Prepare the player to join
+                this._Client.Queue(new SetCompressionPacket(this._Client));
+                this._Client.Queue(new LoginSuccessPacket(this._Client));
+                this._Client.Queue(new JoinGamePacket(this._Client));
+            } else {
+                this._Client.Queue(new DisconnectPacket(ChatComponentFactory.FromFormattedString("Invalid user")), true);
+
+                console.log(`Player ${this._Client.Player.Username} is an invalid offline-mode user`);
+            }
         }
 
         return true;
