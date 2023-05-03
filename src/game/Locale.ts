a815e3c5d31ed0196ad856108b415b0b4032b685
Daniel Hellstern <git@daniel.hellstern.org>
2023-05-03 08:00:26 +0000

    Fixed Joi pattern matching

--- src/game/Locale.ts
+++ src/game/Locale.ts
@@ -13,9 +13,9 @@ export type TranslationMapping = Record<string, Translation>;
 
 export class Locale {
     // YAML file schema
-    private static _Schema: Schema = joi.object().pattern(/[a-zA-Z0-9.]+$/, joi.object({
+    private static _Schema: Schema = joi.object().pattern(/^[a-zA-Z0-9.]+$/, joi.object({
         versions: joi.array().items(joi.alternatives(joi.string(), joi.number())),
-        translations: joi.object().pattern(/[a-z]{2,4}(_[A-Z]{2,4})?/, joi.string()).min(1)
+        translations: joi.object().pattern(/^[a-z]{2,4}(_[A-Z]{2,4})?$/, joi.string()).min(1)
     })).min(1);
 
     // Translation key mapping and metadata
