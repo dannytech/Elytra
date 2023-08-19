3ec931501aa1ab5c2b0d8b2678ed70f9c3f3a3d3
Daniel Hellstern <git@daniel.hellstern.org>
2023-08-19 17:26:22 +0000

    Fixed tests for ChatTextComponentFactory

--- src/game/chat/ChatTextComponentFactory.spec.ts
+++ src/game/chat/ChatTextComponentFactory.spec.ts
@@ -1,8 +1,9 @@
 import test from "ava";
 import { ChatTextComponentFactory } from "./ChatTextComponentFactory";
 import { ChatTextComponent } from "./ChatComponent";
+import { ChatComponentFactory } from "./ChatComponentFactory";
 
-test("Simple component", t => {
+test("Simple text component", t => {
     const component: ChatTextComponent = ChatTextComponentFactory.FromString("Hello, world!");
 
     // Very simple flat structure
@@ -11,57 +12,11 @@ test("Simple component", t => {
     });
 });
 
-test("Complex component", t => {
-    const component = ChatTextComponentFactory.FromFormattedString("&l&eHello, &n&bworld!");
-
-    // Deeply nested structure
-    t.deepEqual(component, {
-        text: "Hello, ",
-        color: "yellow",
-        extra: [
-            {
-                text: "world!",
-                color: "aqua",
-                underline: true
-            }
-        ],
-        bold: true
-    });
-});
-
-test("Complex component with reset", t => {
-    const component = ChatTextComponentFactory.FromFormattedString("&dHello, &r&a&kworld!");
-
-    // Flattened structure due to reset
-    t.deepEqual(component, {
-        text: "",
-        color: "reset",
-        extra: [
-            {
-                text: "Hello, ",
-                color: "light_purple"
-            },
-            {
-                text: "world!",
-                color: "green",
-                obfuscated: true
-            }
-        ]
-    });
-});
-
-test("Raw text from simple component", t => {
+test("Raw text from simple text component", async t => {
     const str = "Hello, world!";
 
     const component: ChatTextComponent = ChatTextComponentFactory.FromString(str);
 
     // Extract the text from the component
-    t.is(ChatTextComponentFactory.GetRaw(component), str);
-});
-
-test("Raw text from complex component", t => {
-    const component = ChatTextComponentFactory.FromFormattedString("&l&6Hello, &r&8world!");
-
-    // Flatten and extract the text from the component
-    t.is(ChatTextComponentFactory.GetRaw(component), "Hello, world!");
+    t.is(await ChatComponentFactory.GetRaw(component), str);
 });
